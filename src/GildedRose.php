368fdba42e8ffd67dda400b565ea91ccbfa9927d
alguive <a.guijarro.vera@gmail.com>
2024-10-21 20:29:08 +0200

    Refactor AgedBrie Item

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace GildedRose;
 
 use GildedRose\Items\AgedBrie;
-use GildedRose\Items\BackstagePasses;
-use GildedRose\Items\Sulfuras;
 
 final class GildedRose
 {
@@ -21,51 +19,53 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-
-            if (!$item instanceof AgedBrie && !$item instanceof BackstagePasses) {
-                if ($item->quality > 0) {
-                    if (!$item instanceof Sulfuras) {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item instanceof BackstagePasses) {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        } else if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!$item instanceof Sulfuras) {
-                $item->sellIn = $item->sellIn - 1;
-            }
-
-            if ($item->sellIn < 0) {
-                if (!$item instanceof AgedBrie) {
-                    if (!$item instanceof BackstagePasses) {
-                        if ($item->quality > 0) {
-                            if (!$item instanceof Sulfuras) {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
+            if ($item instanceof AgedBrie) {
+                $this->manageAgedBrie($item);
             }
         }
     }
+
+    /**
+     * Manage Aged Brie
+     *      Aged Brie actually increases in Quality the older it gets.
+     *      When date expired (SellIn) then the quality (Quality) increases double every day.
+     *
+     * @param Item $item
+     * @return void
+     */
+    protected function manageAgedBrie(Item &$item): void
+    {
+        $this->increaseQuality($item);
+        $this->decreaseSellIn($item);
+
+        if ($item->sellIn < 0) {
+            $this->increaseQuality($item, 1);
+        }
+    }
+
+    /**
+     * Check if Quality is minor than 50, if it is, then increases Quality on 1
+     *
+     * @param Item $item
+     * @return void
+     */
+    protected function increaseQuality(Item &$item): void
+    {
+        if ($item->quality < 50) {
+            $item->quality = $item->quality + 1;
+        }
+    }
+
+    /**
+     * Decreases SellIn  on 1
+     *
+     * @param Item $item
+     * @param integer $quantityToDecrease
+     * @return void
+     */
+    protected function decreaseSellIn(Item &$item): void
+    {
+        $item->sellIn = $item->sellIn - 1;
+    }
+
 }
